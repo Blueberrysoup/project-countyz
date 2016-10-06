8ef27147bee9422cb7bc86ba5d51dadf0caf9746
Carina Ekström <carina.ekstrom@gmail.com>
2016-10-06 15:57:51 +0200

    Final commit

--- src/test/java/com/codebat/TestCountYZ.java
+++ src/test/java/com/codebat/TestCountYZ.java
@@ -21,4 +21,72 @@ public class TestCountYZ {
 		assertEquals(2, yz.countYZ(str));
 	}
 
+	@Test
+	public void testCountYZ2() {
+		String str = "day fez";
+		assertEquals(2, yz.countYZ(str));
+	}
+	
+	@Test
+	public void testCountYZ3() {
+		String str = "day fyyyz";
+		assertEquals(2, yz.countYZ(str));
+	}
+
+	@Test
+	public void testCountYZ4() {
+		String str = "day yak";
+		assertEquals(1, yz.countYZ(str));
+	}
+
+	@Test
+	public void testCountYZ5() {
+		String str = "day:yak";
+		assertEquals(1, yz.countYZ(str));
+	}
+
+	@Test
+	public void testCountYZ6() {
+		String str = "!!day--yaz!!";
+		assertEquals(2, yz.countYZ(str));
+	}
+	
+	@Test
+	public void testCountYZ7() {
+		String str = "yak zak";
+		assertEquals(0, yz.countYZ(str));
+	}
+
+	@Test
+	public void testCountYZ8() {
+		String str = "DAY abc ZYZ";
+		assertEquals(2, yz.countYZ(str));
+	}
+
+	@Test
+	public void testCountYZ9() {
+		String str = "fez day";
+		assertEquals(2, yz.countYZ(str));
+	}
+
+	@Test
+	public void testCountYZ10() {
+		String str = "aaz yyz my";
+		assertEquals(3, yz.countYZ(str));
+	}
+
+	@Test
+	public void testCountYZ11() {
+		String str = "y2bz";
+		assertEquals(2, yz.countYZ(str));
+	}
+
+	@Test
+	public void testCountYZ12() {
+		String str = "zxyx";
+		assertEquals(0, yz.countYZ(str));
+	}
+	
+	
+
 }
